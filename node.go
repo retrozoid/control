b10af206c0dec86bdee560dc014598ddd4c593d8
zoid <aleksandr.dolbichkin@lightspeedhq.com>
2024-04-12 20:25:48 +0400

    update

--- node.go
+++ node.go
@@ -421,7 +421,7 @@ func (e Node) clickablePoint() (middle Point, err error) {
 	if err != nil {
 		return middle, err
 	}
-	_, err = e.frame.evaluate(`new Promise(requestAnimationFrame)`, true)
+	_, err = e.frame.evaluate(`new Promise(requestIdleCallback)`, true)
 	if err != nil {
 		return middle, err
 	}
